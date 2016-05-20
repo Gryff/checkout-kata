9d4dac57b25139a4c6372901cd56c5ffe41c406c
Gryff <gryffskin@gmail.com>
2016-05-20 12:49:38 +0100

    Woops, Product needs a Name property

---
+++ C#/CheckoutKata/CheckoutKata/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckoutKata
+{
+    class Product
+    {
+        public char Name;
+        public int Price;
+        public int DiscountValue;
+        public int DiscountThreshold;
+    }
+}
