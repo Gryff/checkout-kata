c330e58311bc40dfa4d560cf4bf00f2f64d7602d
Gryff <gryffskin@gmail.com>
2016-05-20 16:42:55 +0100

    Remove unnecessary method calls within Catalogue

--- C#/CheckoutKata/CheckoutKata/Catalogue.cs
+++ C#/CheckoutKata/CheckoutKata/Catalogue.cs
@@ -3,26 +3,18 @@ namespace CheckoutKata
     public class Catalogue
     {
         private readonly ProductRepository _products = new ProductRepository();
-        
+
         public int Price(char product) => _products.Find(product).Price;
 
-        public int Discount(char product, int itemCount)
+        public int Discount(char productName, int itemCount)
         {
-            if (!_products.Find(product).HasDiscount()) return 0;
+            var product = _products.Find(productName);
+            if (!product.HasDiscount()) return 0;
 
-            var discount = ProductDiscount(product);
-            var discountThreshold = ProductDiscountThreshold(product);
-
-            if (itemCount >= discountThreshold)
-                return (itemCount/discountThreshold)*discount;
+            if (itemCount >= product.DiscountThreshold)
+                return (itemCount/product.DiscountThreshold)*product.DiscountValue;
 
             return 0;
         }
-
-        private int ProductDiscount(char product) =>
-            _products.Find(product).DiscountValue;
-
-        private int ProductDiscountThreshold(char product) =>
-            _products.Find(product).DiscountThreshold;
     }
 }
