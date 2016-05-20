8fa00c479b4665e43345a128c5ebacbad143b6a7
Gryff <gryffskin@gmail.com>
2016-05-20 15:00:13 +0100

    Products should have a ProductRepository (is this overkill?)

---
+++ C#/CheckoutKata/CheckoutKata/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata
+{
+    public class ProductRepository
+    {
+        private readonly List<Product> _products = new List<Product>
+        {
+            new Product('A', 50, 30, 3),
+            new Product('B', 30, 15, 2),
+            new Product('C', 20, 0, 0),
+            new Product('D', 15, 0, 0)
+        };
+
+        public Product Find(char product) =>
+            _products.First(p => p.Name == product);
+    }
+}
